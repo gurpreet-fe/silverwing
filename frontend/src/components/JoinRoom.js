b18bf5aab9ff88a8d453116eced7c28009c99bc2
Ranjit Singh <sandip.sn131@gmail.com>
2021-01-17 18:20:49 +0000

    Done tutorial 4

---
+++ frontend/src/components/JoinRoom.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class JoinRoom extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <p> This is the Room Join Page </p>;
+  }
+}
