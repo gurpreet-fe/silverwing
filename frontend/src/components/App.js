83cd579d48fe480983113c35b0a06bd74aa30416
RanjitSingh131 <75078460+RanjitSingh131@users.noreply.github.com>
2021-01-17 18:33:41 +0000

    Done tutorial 4 (#6)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import Home from "./Home";
 
 export default class App extends Component {
   constructor(props) {
@@ -7,9 +8,13 @@ export default class App extends Component {
   }
 
   render() {
-    return <h1>Testing React Code</h1>;
+    return (
+      <div>
+        <Home />
+      </div>
+    );
   }
 }
 
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+render(<App name="Gurpreet" />, appDiv);
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import JoinRoom from "./JoinRoom";
+import CreateRoom from "./CreateRoom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <p> This is the Home Page </p>
+          </Route>
+          <Route path="/join" component={JoinRoom} />
+          <Route path="/create" component={CreateRoom} />
+        </Switch>
+      </Router>
+    );
+  }
+}
