5769cdc44f5a26c0789a77fe41bf02654834a55e
RanjitSingh131 <75078460+RanjitSingh131@users.noreply.github.com>
2021-02-07 15:14:25 +0000

    Done tutorial 6 (#8)

--- frontend/src/components/CreateRoom.js
+++ frontend/src/components/CreateRoom.js
@@ -1,10 +1,123 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export default class CreateRoom extends Component {
+  defaultVotes = 2;
+
   constructor(props) {
     super(props);
+    this.state = {
+      guestCanPause: true,
+      votesToSkip: this.defaultVotes,
+    };
+
+    this.roomButtonPressedHandler = this.roomButtonPressedHandler.bind(this);
+    this.votesChangeHandler = this.votesChangeHandler.bind(this);
+    this.guestCanPauseChangeHandler = this.guestCanPauseChangeHandler.bind(
+      this
+    );
   }
+
+  votesChangeHandler(e) {
+    this.setState({
+      votesToSkip: e.target.value,
+    });
+  }
+
+  guestCanPauseChangeHandler(e) {
+    this.setState({
+      guestCanPause: e.target.value === "true" ? true : false,
+    });
+  }
+
+  roomButtonPressedHandler() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        votes_to_skip: this.state.votesToSkip,
+        guest_can_pause: this.state.guestCanPause,
+      }),
+    };
+    fetch("/api/create-room", requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
   render() {
-    return <p> This is the Create Room page </p>;
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography component="h4" variant="h4">
+            Create A Room
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl component="fieldset">
+            <FormHelperText>
+              <div align="center"> Guest control of Playback State </div>
+            </FormHelperText>
+            <RadioGroup
+              row
+              defaultValue="true"
+              onChange={this.guestCanPauseChangeHandler}
+            >
+              <FormControlLabel
+                value="true"
+                control={<Radio color="primary" />}
+                label="Play/Pause"
+                labelPlacement="bottom"
+              />
+              <FormControlLabel
+                value="false"
+                control={<Radio color="secondary" />}
+                label="No Control"
+                labelPlacement="bottom"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl>
+            <TextField
+              required={true}
+              type="number"
+              onChange={this.votesChangeHandler}
+              defaultValue={this.defaultVotes}
+              inputProps={{
+                min: 1,
+                style: { textAlign: "center" },
+              }}
+            />
+            <FormHelperText>
+              <div align="center">Votes Required To Skip Song</div>
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={this.roomButtonPressedHandler}
+          >
+            Create A Room
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="secondary" variant="contained" to="/" component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    );
   }
 }
