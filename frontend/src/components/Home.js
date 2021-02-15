b8d316f2033f2b0e334e4a74e3bfe6ff9d9ff5a7
Ranjit Singh <sandip.sn131@gmail.com>
2021-02-15 19:45:17 +0000

    Work in progress

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,19 +1,44 @@
-import React, { Component } from 'react';
-import JoinRoom from './JoinRoom';
-import CreateRoom from './CreateRoom';
-import Room from './Room';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import JoinRoom from "./JoinRoom";
+import CreateRoom from "./CreateRoom";
+import Room from "./Room";
+import { Button, Grid, Typography, ButtonGroup, Link } from "@material-ui/core";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
   }
+
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align='center'>
+          <Typography variant='h2' compact='h2'>
+            Silver-Wing
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} align='center'>
+          <ButtonGroup disableElevation variant='contained' color='primary'>
+            <Button color='primary' to='/join' component={Link}>
+              Join a Room
+            </Button>
+            <Button color='secondary' to='/create' component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
+
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path='/'>
-            <p> This is the Home Page </p>
+            {this.renderHomePage()}
           </Route>
           <Route path='/join' component={JoinRoom} />
           <Route path='/create' component={CreateRoom} />
