0389863a8c76f0a8c275a73b5be7f764ceb12098
RanjitSingh131 <75078460+RanjitSingh131@users.noreply.github.com>
2021-02-15 21:03:21 +0000

    Done Tutorial 9 (#12)
    * Work in progress
    
    * Done tutorial 9

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,20 +1,73 @@
-import React, { Component } from 'react';
-import JoinRoom from './JoinRoom';
-import CreateRoom from './CreateRoom';
-import Room from './Room';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import JoinRoom from "./JoinRoom";
+import CreateRoom from "./CreateRoom";
+import Room from "./Room";
+import { Button, Grid, Typography, ButtonGroup } from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomCode: null,
+    };
   }
+
+  async componentDidMount() {
+    fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          roomCode: data.code,
+        });
+      });
+  }
+
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align='center'>
+          <Typography variant='h2' compact='h2'>
+            Silver-Wing
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} align='center'>
+          <ButtonGroup disableElevation variant='contained' color='primary'>
+            <Button color='primary' to='/join' component={Link}>
+              Join a Room
+            </Button>
+            <Button color='secondary' to='/create' component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
+
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path='/'>
-            <p> This is the Home Page </p>
-          </Route>
+          <Route
+            exact
+            path='/'
+            render={() => {
+              return this.state.roomCode ? (
+                <Redirect to={`/room/${this.state.roomCode}`} />
+              ) : (
+                this.renderHomePage()
+              );
+            }}
+          />
+
           <Route path='/join' component={JoinRoom} />
           <Route path='/create' component={CreateRoom} />
           <Route path='/room/:roomCode' component={Room} />
